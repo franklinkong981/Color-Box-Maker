5e3e138d52c38bfa8e1f71e2bb54f957191c6591
Franklin Kong <franklinkong981@gmail.com>
2025-01-04 00:33:47 -0800

    Wrote initial BoxList class

---
+++ color-box-maker/src/BoxList.jsx
@@ -0,0 +1,26 @@
+import react, {useState} from "react";
+import {v4 as uuid} from "uuid";
+
+import Box from "./Box.jsx";
+import NewBoxForm from "./NewBoxForm.jsx";
+
+const BoxList = () => {
+  //boxes will be an array of box objects, each box object will have 4 attributes: id, color, height, and width.
+  const [boxes, setBoxes] = useState([]);
+
+  const addBox = (newBox) => {
+    setBoxes(boxes => [...boxes, {...newBox, id: uuid()}]);
+  };
+
+  return (
+    <div className="BoxList">
+      <h2 className="BoxList-title">Box List! Use form to create boxes!</h2>
+      <NewBoxForm addBox={addBox}/>
+      <div className="BoxList-boxes">
+        {boxes.map(box => <Box key={box.id} color={box.color} height={box.height} width={box.width}/>)}
+      </div>
+    </div>
+  );
+};
+
+export default BoxList;
